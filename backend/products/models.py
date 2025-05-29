1e957b5eb539b906bb2f34c8e5864f897e30915d
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-05-29 16:01:28 +0100

    feat: product model and serializers

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveBigIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
