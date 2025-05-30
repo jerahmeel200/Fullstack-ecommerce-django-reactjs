dc2a2ffd837e72fb653b5fb6c950dae59bef1f70
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-05-30 23:34:57 +0100

    feat: products urls, migrations, admin

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.PositiveBigIntegerField()
+    stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='product_images/', null=True, blank=True)   
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
