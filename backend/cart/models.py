55c2be67746775205fe01673317fb618a37e3aa1
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-05-31 01:24:36 +0100

    feat: cart models

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from products.models import Product
 
-# Create your models here.
+User = get_user_model()
+
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
