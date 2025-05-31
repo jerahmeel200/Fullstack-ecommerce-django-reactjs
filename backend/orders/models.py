1353ef516ea4f3efdb1e9251d521ecf405b7be95
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-05-31 10:44:55 +0100

    order models

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from .products.models import Product
+User = get_user_model()
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
