8d2eb3d356b8a6b85fefaa02fb173d690301c244
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-05-31 11:24:56 +0100

    feat: Orders views

--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Order, OrderItem
+from .serializers import OrderSerializer, OrderItemSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class OrderListView(generics.ListAPIView):
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+
