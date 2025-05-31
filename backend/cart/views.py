2b41a5e2c01eeef3889586c286d5444e384b006c
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-05-31 02:05:19 +0100

    feat: cart views

--- backend/cart/views.py
+++ backend/cart/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import CartItem
+from .serializers import CartItemSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class CartListView(generics.ListCreateAPIView):
+    serializer_class = CartItemSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return CartItem.objects.filter(user=self.request.user)
+    
+    
+class AddToCartView(generics.CreateAPIView):
+    serializer_class = CartItemSerializer
+    permission_classes = [IsAuthenticated] 
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+        
+        
+        
+class DeleteCartItemView(generics.DestroyAPIView):
+    serializer_class = CartItemSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        return CartItem.objects.filter(user=self.request.user)           
+    
